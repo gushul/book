1ae3624f25de1ef4c194163798b82b7e947f1687
Dis <sr_5@inbox.ru>
2013-10-19 13:09:43 +0700

    json restaurants view slight modification

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -4,12 +4,22 @@ class Api::RestaurantsController < ApplicationController
   # GET /restaurants.json
   def index
     @restaurants = Restaurant.all
+    @restaurants.each do |r| 
+      r[:tags] = []
+      r.restaurant_tags.each do |t|
+        r[:tags] << t.title
+      end
+    end
     render json: @restaurants 
   end
 
   # GET /restaurants/1.json
   def show
     @restaurant = Restaurant.find(params[:id])
+    @restaurant[:tags] = []
+    @restaurant.restaurant_tags.each do |t|
+      @restaurant[:tags] << t.title
+    end
     render json: @restaurant
   end
     
