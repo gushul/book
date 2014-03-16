918ff1da495429ff879b067324456edc94bb7325
Dis <sr_5@inbox.ru>
2014-03-16 22:54:46 +0700

    new threads for sending email and sms after registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,21 +55,24 @@ class User < ActiveRecord::Base
   end
 
   def send_verification_code_via_sms
-    require 'net/https'
-    require 'open-uri'
-#    uri = URI.parse("https://www.siptraffic.com/myaccount/sendsms.php?username=matthewfong&password=psyagbha&from=+6600000000&to=+#{self.phone}&text=#{self.verify_code}")
-    uri = URI.parse("https://www.siptraffic.com/myaccount/sendsms.php?username=matthewfong&password=psyagbha&from=+66875928489&to=+#{self.phone.reverse.chop.reverse}&text=Welcome+to+Hungry+Hub.+Your+verification+code+is+#{self.verify_code}.+Please+verify+this+number+on+our+webpage+or+in+our+mobile+application.")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    http.get(uri.request_uri)
+    Thread.new do
+      require 'net/https'
+      require 'open-uri'
+      # uri = URI.parse("https://www.siptraffic.com/myaccount/sendsms.php?username=matthewfong&password=psyagbha&from=+6600000000&to=+#{self.phone}&text=#{self.verify_code}")
+      uri = URI.parse("https://www.siptraffic.com/myaccount/sendsms.php?username=matthewfong&password=psyagbha&from=+66875928489&to=+#{self.phone.reverse.chop.reverse}&text=Welcome+to+Hungry+Hub.+Your+verification+code+is+#{self.verify_code}.+Please+verify+this+number+on+our+webpage+or+in+our+mobile+application.")
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http.get(uri.request_uri)
   end
 
 private
 
   def send_verification_code_via_email
-    unless ['development'].include?(Rails.env)
-      UserMailer.registration_confirmation(self.email, self.verify_code).deliver
+    Thread.new do
+      unless ['development'].include?(Rails.env)
+        UserMailer.registration_confirmation(self.email, self.verify_code).deliver
+      end
     end
   end
 
