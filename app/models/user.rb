20646e89a0497bef211b4b57b97533fa1bf3e7af
root <root@Debian-60-squeeze-64-minimal.(none)>
2014-03-17 05:19:47 +0000

    removed private statmeent

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,7 +66,7 @@ class User < ActiveRecord::Base
       http.get(uri.request_uri)
   end
 
-private
+#private
 
   def send_verification_code_via_email
     Thread.new do
@@ -98,4 +98,4 @@ private
     false
   end
 
-end  
+end
