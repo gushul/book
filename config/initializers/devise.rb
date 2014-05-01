f78c4e75c08788d468a8810009ce8d72012a5210
root <root@Debian-60-squeeze-64-minimal.(none)>
2014-05-01 18:20:15 +0000

    fixed facebook login credetials added photos

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -234,7 +234,8 @@ Devise.setup do |config|
       config.omniauth :facebook, '572814159448965', '33db6fbc5a147d434b692fbcd688ce51'
       # config.omniauth :google_oauth2, "APP_ID", "APP_SECRET", { access_type: "offline", approval_prompt: "" }
     when "production"
-      config.omniauth :facebook, "515632468516870", "12eb33be0069042efb658537afdc7f2a"#,
+      config.omniauth :facebook, "215666451922396", "80c8b913e94e0bace9d5dcc7005f91c0"
+#      config.omniauth :facebook, "515632468516870", "12eb33be0069042efb658537afdc7f2a"#,
           # for Heroku
           # {:scope => 'email, offline_access', 
             # :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}} 
