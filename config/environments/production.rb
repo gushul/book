6f74bc50971dfcce7e0bb406eaca2dfd725462f1
murali0861 <murali.kummitha@gmail.com>
2014-07-31 02:01:49 +0530

    UI fix for the box

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,14 +15,16 @@ AuthApp::Application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+    config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
  
   # Generate digests for assets URLs
   config.assets.digest = true
+  
   # config.assets.js_compressor = Uglifier.new(mangle: false)
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
