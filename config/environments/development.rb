f67cb964dfb3c27c45993189517fb87910572dc1
root <root@Debian-60-squeeze-64-minimal.(none)>
2014-09-13 10:10:18 +0000

    truned off emails for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ AuthApp::Application.configure do
   config.action_mailer.default_url_options = { :host => 'hungryhub.com' }
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   # config.action_mailer.smtp_settings = {
