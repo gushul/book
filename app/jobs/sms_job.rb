77a961797cef1d7ea2ecbff39cd6943fe61df26c
root <root@Debian-60-squeeze-64-minimal.(none)>
2014-11-18 17:29:18 +0000

    added photoso

--- app/jobs/sms_job.rb
+++ app/jobs/sms_job.rb
@@ -5,7 +5,8 @@ class SmsJob
     require 'net/https'
     require 'open-uri'
     msg = URI.encode("Welcome to Hungry Hub. Your verification code is #{verify_code}. Please verify this number on our webpage (www.hungryhub.com) or in our mobile application (iOS and Android). Hungry Hub")
-    uri = URI.parse("http://api.rushsms.com:8080/?username=div-hungryhub&password=hBger932&type=0&delivery=1&mobile=66#{phone}&sender=Hungry+Hub&message=#{msg}")
+#    uri = URI.parse("http://api.rushsms.com:8080/?username=div-hungryhub&password=hBger932&type=0&delivery=1&mobile=66#{phone}&sender=Hungry+Hub&message=#{msg}")
+    uri = URI.parse("http://www.thaibulksms.com/sms_api.php?username=hungryhub&password=702287&msisdn=0#{phone}&message=#{msg}&sender=HungryHub")
     http = Net::HTTP.new(uri.host, uri.port)
     # http.use_ssl = true
     # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
